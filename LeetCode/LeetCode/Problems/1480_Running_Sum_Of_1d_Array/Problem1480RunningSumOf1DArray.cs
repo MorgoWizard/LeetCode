5ec34c017e55395f977e668c8a354c1801bb24f6
Nikita <84981163+MorgoWizard@users.noreply.github.com>
2025-01-07 05:08:37 +0300

    ADD: Solving 1480 problem

---
+++ LeetCode/LeetCode/Problems/1480_Running_Sum_Of_1d_Array/Problem1480RunningSumOf1DArray.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Problems._1480_Running_Sum_Of_1d_Array;
+
+public class Problem1480RunningSumOf1DArray {
+    public int[] RunningSum(int[] nums) {
+        int[] result = new int[nums.Length];
+        for (int currentNum = 0; currentNum < nums.Length; currentNum++)
+        {
+            if (currentNum - 1 < 0)
+            {
+                result[currentNum] = nums[currentNum];
+            }
+            else
+            {
+                result[currentNum] = result[currentNum - 1] + nums[currentNum];
+            }
+        }
+        return result;
+    }
+}
---
+++ LeetCode/Tests/Problem1480Test.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Problems._1480_Running_Sum_Of_1d_Array;
+
+
+public class Problem1480Tests
+{
+    private Problem1480RunningSumOf1DArray _problem1480RunningSumOf1DArray;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _problem1480RunningSumOf1DArray = new Problem1480RunningSumOf1DArray();
+    }
+
+    [Test]
+    public void Example1()
+    {
+        int[] input = [1, 2, 3, 4];
+        int[] expected = [1,3,6,10];
+        Assert.That(_problem1480RunningSumOf1DArray.RunningSum(input), Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void Example2()
+    {
+        int[] input = [1,1,1,1,1];
+        int[] expected = [1,2,3,4,5];
+        Assert.That(_problem1480RunningSumOf1DArray.RunningSum(input), Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void Example3()
+    {
+        int[] input = [3,1,2,10,1];
+        int[] expected = [3,4,6,16,17];
+        Assert.That(_problem1480RunningSumOf1DArray.RunningSum(input), Is.EqualTo(expected));
+    }
+}
