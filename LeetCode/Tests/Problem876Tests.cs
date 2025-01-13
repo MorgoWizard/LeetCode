661d7a9ea96a336aa1bc1bb54f7fdf72205209c5
Nikita <84981163+MorgoWizard@users.noreply.github.com>
2025-01-13 22:26:50 +0300

    FEATURE: Problem 876 solved

---
+++ LeetCode/Tests/Problem876Tests.cs
@@ -0,0 +1,57 @@
+﻿using LeetCode.Problems.Problem876;
+
+public class Problem876Tests
+{
+    private Problem876MiddleOfTheLinkedList _problem876MiddleOfTheLinkedList;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _problem876MiddleOfTheLinkedList = new Problem876MiddleOfTheLinkedList();
+    }
+
+    [Test]
+    public void TestListNode()
+    {
+        // Array
+        int[] input = [1, 2, 3];
+        int[] expected = [1, 2, 3];
+        ListNode newListNode = new ListNode(input);
+        
+        //Act
+        int[] result = newListNode.ReturnAllValues();
+        
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void Test1()
+    {
+        // Array
+        int[] input = [1,2,3,4,5];
+        int[] expected = [3,4,5];
+        ListNode newListNode = new ListNode(input);
+        
+        //Act
+        int[] result = _problem876MiddleOfTheLinkedList.MiddleNode(newListNode).ReturnAllValues();
+        
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        // Array
+        int[] input = [1,2,3,4,5,6];
+        int[] expected = [4,5,6];
+        ListNode newListNode = new ListNode(input);
+        
+        //Act
+        int[] result = _problem876MiddleOfTheLinkedList.MiddleNode(newListNode).ReturnAllValues();
+        
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
